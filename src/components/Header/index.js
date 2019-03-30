10a79f3fbe31be7e9ca5a999b97fe57aa21e874e
Rogério Ronald Rossa <rogeriorossa@gmail.com>
2019-03-30 16:20:46 -0300

    alteração do nome do APP

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './styles.css';
 
 const Header = () => (
-    <header id="main-header">JSHunt</header>
+    <header id="main-header">Lista de Produtos</header>
 );
 
 export default Header;
